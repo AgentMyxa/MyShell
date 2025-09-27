af770fb5ff0fadfecf32a5d619d2906ffecf8683
Tima <agent.myxa1@gmail.com>
2025-09-27 16:58:59 +0300

    Configuration

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import getpass
 import socket
+import sys
 import commands
 
 
 username = getpass.getuser()
 hostname = socket.gethostname()
 
+
+def _print_config() -> None:
+    print("Config")
+    print(f"Start script: {sys.argv[0]}")
+    print(f"VFS: {sys.argv[1]}")
+
+
 if __name__ == "__main__":
+    _print_config()
     while True:
         print(f"{username}@{hostname}: ", end="")
         prompt = input()
